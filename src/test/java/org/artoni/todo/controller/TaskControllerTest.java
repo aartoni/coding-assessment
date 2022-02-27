fb4a145739eb62a78cf281705c1ea645ea9a26da
aartoni <arto7796@gmail.com>
2022-02-27 23:43:28 +0100

    Cleanup code, make mock object final

--- src/test/java/org/artoni/todo/controller/TaskControllerTest.java
+++ src/test/java/org/artoni/todo/controller/TaskControllerTest.java
@@ -24,29 +24,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = TasksController.class)
-public class TaskControllerTest {
+class TaskControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    private final MockMvc mockMvc;
 
     @MockBean
     private TasksService tasksService;
 
+    @Autowired
+    TaskControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
     @Test
-    public void shouldAddTask() throws Exception {
+    void shouldAddTask() throws Exception {
         Task task = new Task("Test title", "Test text", null, LocalDateTime.now());
-        this.mockMvc.perform(post("/add-task").with(user("first-user"))
+        mockMvc.perform(post("/add-task").with(user("first-user"))
                         .flashAttr("task", task)).andDo(print())
                 .andExpect(status().is3xxRedirection());
     }
 
     @Test
-    public void shouldRetrieveTasks() throws Exception {
+    void shouldRetrieveTasks() throws Exception {
         List<Task> tasks = new ArrayList<>(1);
         tasks.add(new Task("Test title", "Test text", null, LocalDateTime.now()));
         when(tasksService.getTasks("first-user")).thenReturn(tasks);
 
-        this.mockMvc.perform(get("/tasks").with(user("first-user"))).andDo(print())
+        mockMvc.perform(get("/tasks").with(user("first-user"))).andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Test title")))
                 .andExpect(content().string(containsString("Test text")))
@@ -54,31 +58,31 @@ public class TaskControllerTest {
     }
 
     @Test
-    public void shouldCheckTask() throws Exception {
+    void shouldCheckTask() throws Exception {
         doNothing().when(tasksService).invertTaskStatus(anyLong());
 
-        this.mockMvc.perform(get("/invert-task/1").with(user("first-user"))).andDo(print())
+        mockMvc.perform(get("/invert-task/1").with(user("first-user"))).andDo(print())
                 .andExpect(status().is3xxRedirection());
     }
 
     @Test
-    public void shouldDeleteTask() throws Exception {
+    void shouldDeleteTask() throws Exception {
         doNothing().when(tasksService).removeTask(anyLong());
 
-        this.mockMvc.perform(get("/remove-task/1").with(user("first-user"))).andDo(print())
+        mockMvc.perform(get("/remove-task/1").with(user("first-user"))).andDo(print())
                 .andExpect(status().is3xxRedirection());
     }
 
     @Test
-    public void shouldDisplayTaskCreationPage() throws Exception {
-        this.mockMvc.perform(get("/add-task").with(user("first-user"))).andDo(print())
+    void shouldDisplayTaskCreationPage() throws Exception {
+        mockMvc.perform(get("/add-task").with(user("first-user"))).andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Task creation")));
     }
 
     @Test
-    public void shouldDisplayTaskListPage() throws Exception {
-        this.mockMvc.perform(get("/tasks").with(user("first-user"))).andDo(print())
+    void shouldDisplayTaskListPage() throws Exception {
+        mockMvc.perform(get("/tasks").with(user("first-user"))).andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Tasks list")));
     }
